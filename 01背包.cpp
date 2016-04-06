19676f8c55bd85b4486adbc80c7c0f6c0e4475d2
momosmile <443106364@qq.com>
2016-04-06 20:54:03 +0800

    Create 01背包.cpp

---
+++ 01背包.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+int backpack(int need[], int value[], const int N, const int M)
+{
+    int bp[M + 1];
+    memset(bp, 0, sizeof(bp));
+    for(int i = 0 ; i < N ; ++i)  
+    {  
+        for(int j = M ; j >= need[i] ; --j)  
+        {  
+            bp[j] = max(bp[j] , bp[j-need[i]] + value[i]);  
+        }  
+    }
+    return bp[M];
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    
+    int need[N];
+    int value[N];
+    for(int i = 0; i < N; ++i) 
+    {
+        cin >> need[i] >> value[i];
+    }
+    
+    int res = backpack(need, value, N, M);
+    cout << res << endl;
+    return 0;
+}
